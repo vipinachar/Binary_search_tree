7fcb296c276f2a9fd3b511707d3a52a4f0e279df
Vipin M S <vipinachar2020@gmail.com>
2021-10-23 17:32:14 +0530

    Upto sets

---
+++ a.py
@@ -0,0 +1,58 @@
+# a=10
+# b=10
+# y=a
+# print(id(a))
+# print(id(b))
+# print(id(y))
+
+
+# a=20
+# print(id(a))
+
+
+# and =20
+
+# 12name =20
+
+# _city = "Ranchi"?
+
+# 
+
+
+# a=-1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+# print(type(a))
+
+# a=4.56
+# b=5.1e5
+# print(type(b))?
+
+
+
+
+# a = 5+2j
+# print(type(a))
+
+
+# a = False
+# print(True - False)
+
+# a="hello"
+# b='hello'
+# print(a+b)
+
+
+# a=[1,2,3,4,'hello',4.5]
+# # print(id(a))
+# print(a[-1])
+
+# a[-1]='end'
+# print(a[-1])
+
+
+# a = (1,2,3,'hello')
+# print(a)
+# a[-1] =10 
+
+
+r = range(5,10,2)
+print(r[1])
